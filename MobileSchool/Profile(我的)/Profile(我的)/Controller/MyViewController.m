ff1ed2d1068afe61001b1701be4e6c748c867fb2
Ligongbo <13681258219@163.com>
2016-02-18 16:37:07 +0800

    李公博2.18.2

--- MobileSchool/Profile(我的)/Profile(我的)/Controller/MyViewController.m
+++ MobileSchool/Profile(我的)/Profile(我的)/Controller/MyViewController.m
@@ -61,33 +61,33 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    NSString* text = [[_dataSource objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
-    
-    if ([text isEqualToString:@"系统设置"]||[text isEqualToString:@"我的订单"]||[text isEqualToString:@"地址管理"]||[text isEqualToString:@"我的账号"]) {
-        
-        
-    }
-    
-    
-    if ([text isEqualToString:@"系统设置"]) {
-        
-    }
-    
-    if ([text isEqualToString:@"我的订单"]) {
-        
-    }
-    if ([text isEqualToString:@"地址管理"]) {
-        
-    }
-    if ([text isEqualToString:@"我的账号"]) {
-        
-    }
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+//    NSString* text = [[_dataSource objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
+//    
+//    if ([text isEqualToString:@"系统设置"]||[text isEqualToString:@"我的订单"]||[text isEqualToString:@"地址管理"]||[text isEqualToString:@"我的账号"]) {
+//        
+//        
+//    }
+//    
+//    
+//    if ([text isEqualToString:@"系统设置"]) {
+//        
+//    }
+//    
+//    if ([text isEqualToString:@"我的订单"]) {
+//        
+//    }
+//    if ([text isEqualToString:@"地址管理"]) {
+//        
+//    }
+//    if ([text isEqualToString:@"我的账号"]) {
+//        
+//    }
+//    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (indexPath.section == 0 && indexPath.row == 1) {
-        return 100.f;
+    if (indexPath.section == 0 && indexPath.row == 0) {
+        return 200.f;
     }
     return 44.f;
 }
