67bfd50005b1e0fb964e536522c0425c2741609b
Ligongbo <13681258219@163.com>
2016-02-23 11:41:17 +0800

    setting

--- MobileSchool/Profile(我的)/Setting(设置)/Controllers/SettingViewController.h
+++ MobileSchool/Profile(我的)/Setting(设置)/Controllers/SettingViewController.h
@@ -2,12 +2,12 @@
 //  SettingViewController.h
 //  MobileSchool
 //
-//  Created by 李公博 on 16/2/20.
+//  Created by 李公博 on 16/2/23.
 //  Copyright © 2016年 Molly. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface SettingViewController : UIViewController
+@interface SettingViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
 
 @end
